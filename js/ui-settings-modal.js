f02c61102b2dacdb126a654bb945b43f32ca7797
gevikhn <gevikhn@gmail.com>
2025-09-10 13:42:01 +0800

    chore: hide collapse toggle on narrow screens

--- js/ui-settings-modal.js
+++ js/ui-settings-modal.js
@@ -54,10 +54,21 @@ function hasActiveSubOverlay(){
   return (!!u && !u.hidden) || (!!m && !m.hidden);
 }
 
-try {
-  const initCollapsed = localStorage.getItem(COLLAPSE_KEY) === '1';
-  applyActionsCollapsed(initCollapsed);
-} catch {}
+const mqActions = window.matchMedia('(max-width:820px)');
+function syncActionsCollapse(){
+  if (mqActions.matches){
+    applyActionsCollapsed(false);
+  } else {
+    try {
+      const initCollapsed = localStorage.getItem(COLLAPSE_KEY) === '1';
+      applyActionsCollapsed(initCollapsed);
+    } catch {
+      applyActionsCollapsed(false);
+    }
+  }
+}
+syncActionsCollapse();
+mqActions.addEventListener('change', syncActionsCollapse);
 if (toggleActions){
   toggleActions.addEventListener('click', ()=>{
     const now = !(actionsPanel && actionsPanel.classList.contains('collapsed'));
